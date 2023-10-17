d9a41db56f5e4ab8e091562963b633a9fcb5d26a
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-10-17 11:36:13 +0330

    Complete Step 1

--- Polymorphism/Polymorphism/Program.cs
+++ Polymorphism/Polymorphism/Program.cs
@@ -4,6 +4,43 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Correct
+        Animal cat = new Cat();
+        cat.Sound();//Print "General Sound"
+
+        //Correct
+        Dog dog = new Dog();
+        dog.Sound();//Print "General Sound" , "Hop Hop"
+
+        //Incorrect
+        //Cat cat = new Animal();
+        //Dog dog = new Animal();
+
+        Console.ReadKey();
+    }
+}
+
+public class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("General Sound");
+    }
+}
+
+public class Cat : Animal
+{
+    public void Sound()
+    {
+        Console.WriteLine("Mio Mio");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void Sound()
+    {
+        base.Sound();
+        Console.WriteLine("Hop Hop");
     }
 }
